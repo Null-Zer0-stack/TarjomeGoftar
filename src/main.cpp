fa0ecdbba039cbf067fc31b6beb4aa6b8e35e705
Null-Zer0-stack <52693491+Null-Zer0-stack@users.noreply.github.com>
2025-10-25 18:14:29 +0330

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@ int main(int argc, char *argv[])
 {
     
     QApplication a(argc, argv);
-   // QString app_name = "TarjomeGoftar";
-   // a.applicationName = app_name;
     MainWindow w;
     w.show();
     w.setWindowTitle("TarjomeGoftar");
     return a.exec();
 }
+
 
 
